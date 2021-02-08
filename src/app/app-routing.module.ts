e0db7d02cc66010a74b48eea334b3760cc2d8cd2
Dcterrance88 <>
2021-02-07 21:21:31 -0500

    Creando rutas hijas para heroes y auth
    para heroes y auth se creo un componente para añadir y configurar las
    rutas hijas, para luego añadirse al modulo de heroes y auth con el fin
    de que queden funcionales.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,14 @@ import { RouterModule, Routes } from '@angular/router'
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 const routes: Routes = [
+  {//1
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule)
+  },
+  {
+    path: 'heroes',
+    loadChildren: () => import('./heroes/heroes.module').then( m => m.HeroesModule)
+  },
   {
     path: '404',
     component: ErrorPageComponent
@@ -23,3 +31,9 @@ const routes: Routes = [
   ]
 })
 export class AppRoutingModule { }
+
+/*
+Cuando alguien entre al path auth carga sus hijos y dichos
+va a venir del producto del auth.module, cuando se carge en 
+memoria, entonces el modulo que va a regresar es el AuthModule.
+*/
